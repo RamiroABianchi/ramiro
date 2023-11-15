5c18d8063a17d30e07cb83485fe7b783ee091d29
RamiroABianchi <147105141+RamiroABianchi@users.noreply.github.com>
2023-11-15 16:40:03 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,7 +1,7 @@
 <script>
     function scrollDown() {
       // Selecciona el elemento al que se hace scroll
-      var targetElement = document.getElementById('elementoDestino'); 
+      var targetElement = document.getElementById('contacto'); 
 
       // Se Utiliza scrollIntoView para hacer scroll suave hacia el elemento
       targetElement.scrollIntoView({
